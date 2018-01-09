399df6a217e76a5fabf46ce991b26376269a348d
Jennifer Buchanan <jenniferb.1230@gmail.com>
2018-01-09 00:46:42 -0500

    header changes

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,7 +6,6 @@ import React from 'react';
 		return (
 		<div className="row">	
 			<header className="nav-bar">
-
 			<p>Chappy Nest</p>
 				<ul>
 					<a href={'/' + props.link1}><li>{props.link1Name}</li></a>
