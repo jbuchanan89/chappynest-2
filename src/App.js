9e070704e98fa40e38e898fc280c188630eca453
Jennifer Buchanan <jenniferb.1230@gmail.com>
2018-01-11 16:10:47 -0500

    added new home page changed colors and routes

--- src/App.js
+++ src/App.js
@@ -2,12 +2,19 @@ import React, { Component } from "react";
 import NewHomePage from "./components/NewHomePage";
  
 class App extends Component {
+	loginUser(user,cb){
+		localStorage.setItem('ChappyNest-userID',user._id);
+		localStorage.setItem('ChappyNest-userType',user.type);
+		localStorage.setItem('ChappyNest-name',user.name);
+		cb();
+		// this.setState({user: user});
+	}
 
   render() {
     
     return (
       <div className="App">
-      <NewHomePage loginUser={this.loginUser} {...props}/>
+      <NewHomePage loginUser={this.loginUser}/>
       </div>
     );
   }
